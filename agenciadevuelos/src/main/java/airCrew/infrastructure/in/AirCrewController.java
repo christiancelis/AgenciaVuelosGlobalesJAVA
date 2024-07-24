10a99f93b106ad3480431ad3143a72dc544a33fa
camih1005 <camiloht0918@gmail.com>
2024-07-24 17:07:41 -0500

    ultimo commit 2:00 pm

--- agenciadevuelos/src/main/java/airCrew/infrastructure/in/AirCrewController.java
+++ agenciadevuelos/src/main/java/airCrew/infrastructure/in/AirCrewController.java
@@ -12,7 +12,6 @@ import airCrew.application.UseCaseValidateAirCrew;
 import airCrew.domain.AirCrew;
 import airline.application.UseCaseGetAllAirlines;
 import airline.domain.Airline;
-
 import employee.application.UseCaseGetAllEmployees;
 import employee.application.UseCaseGetEmployeeByAirline;
 import employee.application.UseCaseGetEmployeeById;
@@ -20,7 +19,6 @@ import employee.domain.Employee;
 import utils.Validation;
 import viaje.application.ListVuelos;
 import viaje.domain.FlightRecord;
-
 
 public class AirCrewController {
     private final UseCaseAddCrewToTrip useCaseAddCrewToTrip;
@@ -35,10 +33,10 @@ public class AirCrewController {
     Scanner scanner = new Scanner(System.in);
 
     public AirCrewController(UseCaseAddCrewToTrip useCaseAddCrewToTrip, UseCaseGetAirCrewToTrip useCaseGetAirCrewToTrip,
-            UseCaseGetAllEmployees useCaseGetAllEmployees, UseCaseGetIdAirlineOfTrip useCaseGetIdAirlineOfTrip,
-            ListVuelos useCaseGetAllVuelos, UseCaseGetEmployeeByAirline useCaseGetEmployeeByAirline,
-            UseCaseGetAllAirlines useCaseGetAllAirlines, UseCaseGetEmployeeById useCaseGetEmployeeById,
-            UseCaseValidateAirCrew useCaseValidateAirCrew) {
+                             UseCaseGetAllEmployees useCaseGetAllEmployees, UseCaseGetIdAirlineOfTrip useCaseGetIdAirlineOfTrip,
+                             ListVuelos useCaseGetAllVuelos, UseCaseGetEmployeeByAirline useCaseGetEmployeeByAirline,
+                             UseCaseGetAllAirlines useCaseGetAllAirlines, UseCaseGetEmployeeById useCaseGetEmployeeById,
+                             UseCaseValidateAirCrew useCaseValidateAirCrew) {
         this.useCaseAddCrewToTrip = useCaseAddCrewToTrip;
         this.useCaseGetAirCrewToTrip = useCaseGetAirCrewToTrip;
         this.useCaseGetAllEmployees = useCaseGetAllEmployees;
@@ -53,136 +51,163 @@ public class AirCrewController {
     public void AsignarTripulacionViaje(){
         int idAerolinea;
         imprimirViajes();
-        int idViaje = Validation.leerNumero("Digite el id del viaje", scanner);
+        int idViaje = Validation.leerNumero("Digite el ID del viaje", scanner);
         Boolean existeViaje = verificarExistenciaViaje(idViaje);
-        if(existeViaje==false){System.out.println("id Viaje no valido"); return;}
+        if(!existeViaje){
+            System.out.println("ID del viaje no válido");
+            return;
+        }
 
-        int idAeroViaje= useCaseGetIdAirlineOfTrip.execute(idViaje);
-        if(idAeroViaje==0){
+        int idAeroViaje = useCaseGetIdAirlineOfTrip.execute(idViaje);
+        if(idAeroViaje == 0){
             imprimirAerolienas();
-            idAerolinea = Validation.leerNumero("Digite el id de la aerolinea", scanner);
+            idAerolinea = Validation.leerNumero("Digite el ID de la aerolínea", scanner);
             Boolean existe = verificarExistenciaAerolinea(idAerolinea);
-            if(existe==false){System.out.println("id Aerolinea no valido"); return;}
-        }else{
-            ArrayList <Airline> listaAerolineas = useCaseGetAllAirlines.execute(); 
-            Optional <Airline> obj = listaAerolineas.stream().filter((e)->e.getIdAirline()==idAeroViaje).findFirst();
-            if(!obj.isPresent()){System.out.println("No se puedo obtener la aerolinea"); return;}
+            if(!existe){
+                System.out.println("ID de aerolínea no válido");
+                return;
+            }
+        } else {
+            ArrayList<Airline> listaAerolineas = useCaseGetAllAirlines.execute();
+            Optional<Airline> obj = listaAerolineas.stream().filter(e -> e.getIdAirline() == idAeroViaje).findFirst();
+            if(!obj.isPresent()){
+                System.out.println("No se pudo obtener la aerolínea");
+                return;
+            }
             Airline aerolinea = obj.get();
-            System.out.println("La tripulacion del viaje pertenecen a la aerolinea " + aerolinea.getNombreAirline());
+            System.out.println("La tripulación del viaje pertenece a la aerolínea " + aerolinea.getNombreAirline());
             idAerolinea = idAeroViaje;
         }
 
         ArrayList<Employee> listaEmpleados = useCaseGetAirCrewToTrip.execute(idViaje);
-        if(listaEmpleados==null){
+        if(listaEmpleados == null){
             imprimirEmpleadosPorAerolinea(idAerolinea);
-            int idEmpleado = Validation.leerNumero("Digite el id del empleado", scanner);
-            boolean existeEmpleado = verificarEmpleadoEnAerolinea(idEmpleado,idAerolinea);
-            if(existeEmpleado==false){System.out.println("Empleado no se encuentra en la aerolinea"); return;}
+            int idEmpleado = Validation.leerNumero("Digite el ID del empleado", scanner);
+            boolean existeEmpleado = verificarEmpleadoEnAerolinea(idEmpleado, idAerolinea);
+            if(!existeEmpleado){
+                System.out.println("El empleado no se encuentra en la aerolínea");
+                return;
+            }
             AirCrew tripulacion = new AirCrew();
             tripulacion.setIdEmpleado(idEmpleado);
             tripulacion.setIdViaje(idViaje);
             useCaseAddCrewToTrip.execute(tripulacion);
-            System.out.println("Empleado Agregado a tripulacion");
-        }else{
-            System.out.println("Agregar Tripulacion: ");
+            System.out.println("Empleado agregado a la tripulación");
+        } else {
+            System.out.println("Agregar tripulación: ");
             imprimirEmpleadosPorAerolinea(idAerolinea);
-            int idEmpleado = Validation.leerNumero("Digite el id del empleado", scanner);
+            int idEmpleado = Validation.leerNumero("Digite el ID del empleado", scanner);
             boolean existeEmpleado = verificarEmpledoEnViaje(idEmpleado, idViaje);
-            if(existeEmpleado==true){System.out.println("Empleado ya se encuentra asignado"); return;}
-            boolean existeEmp = verificarEmpleadoEnAerolinea(idEmpleado,idAerolinea);
-            if(existeEmp==false){System.out.println("Empleado no se encuentra en la aerolinea"); return;}
+            if(existeEmpleado){
+                System.out.println("El empleado ya se encuentra asignado");
+                return;
+            }
+            boolean existeEmp = verificarEmpleadoEnAerolinea(idEmpleado, idAerolinea);
+            if(!existeEmp){
+                System.out.println("El empleado no se encuentra en la aerolínea");
+                return;
+            }
             
             AirCrew tripulacion = new AirCrew();
             tripulacion.setIdEmpleado(idEmpleado);
             tripulacion.setIdViaje(idViaje);
             useCaseAddCrewToTrip.execute(tripulacion);
-            System.out.println("Empleado Agregado a tripulacion");
+            System.out.println("Empleado agregado a la tripulación");
         }
     }
 
     public void consultarAsignacionTripulacion(){
         imprimirEmpleados();
-        int idEmpleado = Validation.leerNumero("Digite el id del empleado", scanner);
+        int idEmpleado = Validation.leerNumero("Digite el ID del empleado", scanner);
 
         AirCrew tripulacion = useCaseValidateAirCrew.execute(idEmpleado);
-        if(tripulacion==null){System.out.println("El empleado no se encuentra en una tripulacion"); return;}
+        if(tripulacion == null){
+            System.out.println("El empleado no se encuentra en una tripulación");
+            return;
+        }
 
         ArrayList<Employee> listEmpleado = useCaseGetAllEmployees.execute();
-        Optional <Employee> objemp = listEmpleado.stream().filter((e)->e.getId()==idEmpleado).findFirst();
+        Optional<Employee> objemp = listEmpleado.stream().filter(e -> e.getId() == idEmpleado).findFirst();
         Employee empleado = objemp.get();   
-        ArrayList <Airline> listaAerolinea = useCaseGetAllAirlines.execute();
-        Optional <Airline> objAerolinea = listaAerolinea.stream().filter((e)->e.getIdAirline()==empleado.getIdAirline()).findFirst();
+        ArrayList<Airline> listaAerolinea = useCaseGetAllAirlines.execute();
+        Optional<Airline> objAerolinea = listaAerolinea.stream().filter(e -> e.getIdAirline() == empleado.getIdAirline()).findFirst();
         Airline aerolinea = objAerolinea.get();
         ArrayList<FlightRecord> listaViajes = useCaseGetAllVuelos.execute();
-        Optional <FlightRecord> objViaje = listaViajes.stream().filter((e)-> e.getId()==tripulacion.getIdViaje()).findFirst();
+        Optional<FlightRecord> objViaje = listaViajes.stream().filter(e -> e.getId() == tripulacion.getIdViaje()).findFirst();
         FlightRecord viaje = objViaje.get();
-        System.out.println("Empleado en tripulacion");
-        System.out.println("Aerolinea: " + aerolinea.getNombreAirline());
-        System.out.println("Nombre Empleado: " + empleado.getNombre());
-        System.out.println("Viaje Asignado: ");
-        System.out.println("Ciudad de origen: "+ viaje.getOriginCity());
-        System.out.println("Ciudad destino" + viaje.getDestinationCity());
+        System.out.println("Empleado en tripulación:");
+        System.out.println("========================");
+        System.out.println("Aerolínea: " + aerolinea.getNombreAirline());
+        System.out.println("Nombre del empleado: " + empleado.getNombre());
+        System.out.println("Viaje asignado: ");
+        System.out.println(" - Ciudad de origen: " + viaje.getOriginCity());
+        System.out.println(" - Ciudad destino: " + viaje.getDestinationCity());
     }
 
-
-  private boolean verificarExistenciaAerolinea(int idAerolinea){
+    private boolean verificarExistenciaAerolinea(int idAerolinea){
         ArrayList<Airline> listaAerolineas = useCaseGetAllAirlines.execute();
-        Boolean existe = listaAerolineas.stream().anyMatch((e)-> e.getIdAirline()==idAerolinea);
-        if(existe){return true;}
-        return false;
+        Boolean existe = listaAerolineas.stream().anyMatch(e -> e.getIdAirline() == idAerolinea);
+        return existe;
     }
 
-
-private boolean verificarEmpleadoEnAerolinea(int idEmpleado, int idAerolinea){
-        ArrayList <Employee> listaEmpleados = useCaseGetEmployeeByAirline.execute(idAerolinea);
-        Boolean existe = listaEmpleados.stream().anyMatch((e)-> e.getId()==idEmpleado);
-        if(existe){return true;}
-        return false;
-}
+    private boolean verificarEmpleadoEnAerolinea(int idEmpleado, int idAerolinea){
+        ArrayList<Employee> listaEmpleados = useCaseGetEmployeeByAirline.execute(idAerolinea);
+        Boolean existe = listaEmpleados.stream().anyMatch(e -> e.getId() == idEmpleado);
+        return existe;
+    }
 
     private boolean verificarEmpledoEnViaje(int idEmpleado, int idViaje){
-        ArrayList <Employee> listaEmpleados = useCaseGetAirCrewToTrip.execute(idViaje);
-        Boolean existe = listaEmpleados.stream().anyMatch((e)-> e.getId()==idEmpleado);
-        if(existe){return true;}
-        return false;
+        ArrayList<Employee> listaEmpleados = useCaseGetAirCrewToTrip.execute(idViaje);
+        Boolean existe = listaEmpleados.stream().anyMatch(e -> e.getId() == idEmpleado);
+        return existe;
     }
 
-    
-  private boolean verificarExistenciaViaje(int idViaje){
-    ArrayList<FlightRecord> listaViajes = useCaseGetAllVuelos.execute();
-    Boolean existe = listaViajes.stream().anyMatch((e)-> e.getId()==idViaje);
-    if(existe){return true;}
-    return false;
-}
-
+    private boolean verificarExistenciaViaje(int idViaje){
+        ArrayList<FlightRecord> listaViajes = useCaseGetAllVuelos.execute();
+        Boolean existe = listaViajes.stream().anyMatch(e -> e.getId() == idViaje);
+        return existe;
+    }
 
     private void imprimirAerolienas(){
-        ArrayList <Airline> listaAerolineas = useCaseGetAllAirlines.execute();
-        Consumer <Airline> imprimirAerolineas = (e)-> System.out.println("Id: " + e.getIdAirline() + " Nombre:" + e.getNombreAirline());
-        listaAerolineas.forEach(imprimirAerolineas);
+        ArrayList<Airline> listaAerolineas = useCaseGetAllAirlines.execute();
+        System.out.println("=== Lista de Aerolíneas ===");
+        listaAerolineas.forEach(e -> System.out.println("ID: " + e.getIdAirline() + " | Nombre: " + e.getNombreAirline()));
+        System.out.println("===========================");
     }
 
     private void imprimirEmpleadosPorAerolinea(int idAerolinea){
-        ArrayList <Employee> listaEmpleados = useCaseGetEmployeeByAirline.execute(idAerolinea);
-        Consumer <Employee> imprimirEmpleados = (e)-> System.out.println("Id: " + e.getId() + " Nombre:" + e.getNombre());
-        listaEmpleados.forEach(imprimirEmpleados);
+        ArrayList<Employee> listaEmpleados = useCaseGetEmployeeByAirline.execute(idAerolinea);
+        System.out.println("=== Lista de Empleados de la Aerolínea ===");
+        listaEmpleados.forEach(e -> System.out.println("ID: " + e.getId() + " | Nombre: " + e.getNombre()));
+        System.out.println("===========================================");
+    }
+
+    private void imprimirViajes() {
+        ArrayList<FlightRecord> listaViajes = useCaseGetAllVuelos.execute();
+        
+        System.out.println("=================================");
+        System.out.println("       Listado de Vuelos         ");
+        System.out.println("=================================");
+        System.out.println("ID    | Ciudad Origen             | Aeropuerto Origen         | Ciudad Destino            | Aeropuerto Destino");
+        System.out.println("----------------------------------------------------------");
+
+        listaViajes.forEach(v -> {
+            System.out.printf("%-5d | %-25s | %-25s | %-25s | %-25s%n", 
+                v.getId(), 
+                v.getOriginCity(), 
+                v.getOriginAirport(), 
+                v.getDestinationCity(), 
+                v.getDestinationAirport()
+            );
+        });
+        
+        System.out.println("=================================");
     }
 
     private void imprimirEmpleados(){
-        ArrayList <Employee> listaEmpleados = useCaseGetAllEmployees.execute();
-        Consumer <Employee> imprimirEmpleados = (e)-> System.out.println("Id: " + e.getId() + " Nombre:" + e.getNombre());
-        listaEmpleados.forEach(imprimirEmpleados);
+        ArrayList<Employee> listaEmpleados = useCaseGetAllEmployees.execute();
+        System.out.println("=== Lista de Empleados ===");
+        listaEmpleados.forEach(e -> System.out.println("ID: " + e.getId() + " | Nombre: " + e.getNombre()));
+        System.out.println("===========================");
     }
-
-    private void imprimirViajes(){
-        ArrayList <FlightRecord> listaViajes = useCaseGetAllVuelos.execute();
-        Consumer <FlightRecord> imprimirViaje = (e)-> System.out.println("Id: " + e.getId() + " Ciudad Origen:" + e.getOriginCity() + " Ciudad Destino" + e.getDestinationCity());
-        listaViajes.forEach(imprimirViaje);
-    }
-
-
-
-
-
-    
 }
